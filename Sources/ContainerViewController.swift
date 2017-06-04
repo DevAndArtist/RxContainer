37044f8cbf934d4771fcd52dd365105b302536ab
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-04 17:30:19 +0200

    Stub for `ContainerViewController`.

---
+++ Sources/ContainerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContainerViewController.swift
+//  RxContainer
+//
+//  Created by Adrian Zubarev on 04.06.17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+import UIKit
+
+open class ContainerViewController : UIViewController {
+
+}
