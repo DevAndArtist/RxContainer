47702e0d3354a7cf271fee351e1b60f1c24c50c9
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-04 21:26:53 +0200

    More comments and basic logic for `viewControllers` setter.

--- Sources/ContainerViewController.swift
+++ Sources/ContainerViewController.swift
@@ -12,24 +12,42 @@ import UIKit
 open class ContainerViewController : UIViewController {
 
 	// internal properties
+	var viewControllerStack = [UIViewController]()
 	let disposeBag = DisposeBag()
 	let eventsSubject = PublishSubject<ContainerViewController.Event>()
 
 	// open/public properties
-	open var viewControllers: [UIViewController] = []
+	/// The view controllers currently on the view controller stack.
+	///
+	/// The root view controller is at index `0` in the array and the top 
+	/// controller is at index n-1, where n is the number of items in the array.
+	///
+	/// Assigning a new array of view controllers to this property is equivalent to 
+	/// calling the `setViewControllers(_:animated:)` method with the `animated` parameter
+	/// set to `false`.
+	open var viewControllers: [UIViewController] {
+		get { return self.viewControllerStack }
+		set { self.setViewControllers(newValue, animated: false) }
+	}
 
+	/// The root view controller of the view controller stack.
 	open var rootViewController: UIViewController? {
 		return self.viewControllers.first
 	}
 
+	/// The view controller at the top of the view controller stack.
 	open var topViewController: UIViewController? {
 		return self.viewControllers.last
 	}
 
-	/// Once the syntax sugar from SE-0111 commentary
-	/// (https://lists.swift.org/pipermail/swift-evolution-announce/2016-July/000233.html)
+	/// Once the syntax sugar from [SE-0111 commentary
+	/// ](https://lists.swift.org/pipermail/swift-evolution-announce/2016-July/000233.html)
 	/// made into the language, this closure will become either
-	///    - `var animator(for:): (Transition) -> Animator?` or
+	///
+	///    - `var animator(for:): (Transition) -> Animator?`
+	///
+	/// or
+	///
 	///    - `var animator: (for: Transition) -> Animator?`
 	open var animator /* (for:) */: (Transition) -> Animator? = {
 		_ in
