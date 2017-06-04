18a4f2723ec71a3611e30d5304964bd54c5976c3
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-04 16:07:55 +0200

    Added project base.

---
+++ RxContainer/RxContainer.h
@@ -0,0 +1,18 @@
+//
+//  RxContainer.h
+//  RxContainer
+//
+//  Created by Adrian Zubarev on 04.06.17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxContainer.
+FOUNDATION_EXPORT double RxContainerVersionNumber;
+
+//! Project version string for RxContainer.
+FOUNDATION_EXPORT const unsigned char RxContainerVersionString[];
+
+// In this header, you should import all the public headers of your
+// framework using statements like #import <RxContainer/PublicHeader.h>
