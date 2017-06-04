0681035325e8419e6ecd1cde5411a94b930f4844
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-04 18:05:28 +0200

    Added internal `ViewComponent` protocol.

---
+++ Sources/ViewComponent.swift
@@ -0,0 +1,15 @@
+//
+//  ViewComponent.swift
+//  RxContainer
+//
+//  Created by Adrian Zubarev on 04.06.17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewComponent {
+
+	func viewController(forKey key: ViewComponentKey) -> UIViewController
+	func view(forKey key: ViewComponentKey) -> UIView
+}
