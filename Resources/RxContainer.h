9b6fd9c7878e2ca0c907815e218e72cc9f311551
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-04 21:27:25 +0200

    Cleaned up comments.

--- Resources/RxContainer.h
+++ Resources/RxContainer.h
@@ -1,18 +1,12 @@
-//
-//  RxContainer.h
-//  RxContainer
-//
-//  Created by Adrian Zubarev on 04.06.17.
-//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
-//
+///
+///  Created by Adrian Zubarev on 04.06.17.
+///  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+///
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for RxContainer.
+/// Project version number for RxContainer.
 FOUNDATION_EXPORT double RxContainerVersionNumber;
 
-//! Project version string for RxContainer.
+/// Project version string for RxContainer.
 FOUNDATION_EXPORT const unsigned char RxContainerVersionString[];
-
-// In this header, you should import all the public headers of your
-// framework using statements like #import <RxContainer/PublicHeader.h>
