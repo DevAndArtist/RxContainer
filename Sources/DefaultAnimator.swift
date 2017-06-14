ba2dd94831b9230c7a271196b30d34c7eb732ef6
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-14 18:34:40 +0200

    Prepare first parts for `DefaultAnimator`.

--- Sources/DefaultAnimator.swift
+++ Sources/DefaultAnimator.swift
@@ -15,6 +15,40 @@ public final class DefaultAnimator : Animator {
 		case left, right, top, bottom
 	}
 
+	//
+	var context: Transition.Context {
+		return self.transition.context
+	}
+
+	var optionalAnimation: ((Transition.Context) -> Void)? {
+		return self.transition.animation
+	}
+
+	var optionalCompletion: ((Transition.Context) -> Void)? {
+		return self.transition.completion
+	}
+
+	var containerView: UIView {
+		return self.context.containerView
+	}
+
+	var fromView: UIView {
+		return self.context.view(forKey: .from)
+	}
+
+	var toView: UIView {
+		return self.context.view(forKey: .to)
+	}
+
+	var shouldPush: Bool {
+		return self.context.kind == .push
+	}
+
+	var views: [UIView] {
+		return self.shouldPush ? [self.fromView, self.toView] : [self.toView, self.fromView]
+	}
+
+	//
 	///
 	public let transition: Transition
 
@@ -28,28 +62,33 @@ public final class DefaultAnimator : Animator {
 		self.direction = direction
 	}
 
+	func finalState() {
+
+		if self.context.isAnimated {
+			self.optionalAnimation?(self.context)
+		} else {
+			UIView.performWithoutAnimation { self.optionalAnimation?(self.context) }
+		}
+	}
+
 	///
 	public func animate() {
 
-		let context = self.transition.context
-		let optionalAnimation = self.transition.animation
-
-		func finalState() {
-
-			if context.isAnimated {
-				optionalAnimation?(context)
-			} else {
-				UIView.performWithoutAnimation { optionalAnimation?(context) }
-			}
+		self.views.forEach {
+			$0.autoresizingMask = .complete
+			$0.translatesAutoresizingMaskIntoConstraints = true
+			$0.removeFromSuperview()
+			$0.frame = self.containerView.bounds
+			self.containerView.addSubview($0)
 		}
 
 		func complete(_ didComplete: Bool) {
 			self.transition.complete(didComplete)
-			self.transition.completion?(context)
+			self.optionalCompletion?(self.context)
 		}
 
 		if context.isAnimated {
-			UIView.animate(withDuration: 0.3, animations: finalState, completion: complete)
+			UIView.animate(withDuration: 0.3, animations: self.finalState, completion: complete)
 		} else {
 			finalState()
 			complete(true)
