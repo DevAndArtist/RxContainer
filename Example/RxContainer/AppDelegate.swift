32a56c5f5f44bc0e290b36a69760d8ba7bd61678
Adrian Zubarev <adrian.zubarev@devandartist.com>
2017-06-15 20:55:28 +0200

    Added an example project.

---
+++ Example/RxContainer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RxContainer
+//
+//  Created by Adrian Zubarev on 15.06.17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate : UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+}
+
+extension AppDelegate {
+
+	func application(_ application: UIApplication,
+	                 didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		window.rootViewController = ViewController()
+		window.makeKeyAndVisible()
+		self.window = window
+		return true
+	}
+}
---
+++ Example/RxContainer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RxContainer
+//
+//  Created by Adrian Zubarev on 15.06.17.
+//  Copyright © 2017 RxSwiftCommunity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController : UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = .orange
+	}
+}
